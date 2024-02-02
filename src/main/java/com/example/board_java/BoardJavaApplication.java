8e24cec4d0d855ce7be585c0ec716e7b2c0222ba
seokhwan2 <waw7095@gmail.com>
2024-02-02 15:15:27 +0900

    Java-board 시작

---
+++ src/main/java/com/example/board_java/BoardJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardJavaApplication.class, args);
+    }
+
+}
